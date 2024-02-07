6dd4a80a4063571b6d90bf59365c71dda010740e
aurma01 <aurma01@student.bbk.ac.uk>
2024-02-07 16:07:17 +0200

    create signin backend route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     //get username, email and password from request body
@@ -16,6 +18,30 @@ export const signup = async (req, res, next) => {
         res.status(201).json({ message : 'User created successfully'});
     } catch (error) {
         //send error message to client if user is not created successfully
+        next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    //get email and password from request body
+    const {email, password} = req.body;
+    try {
+        //check if the user exist
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(401, 'Invalid credentials'));
+        //check if the password is correct
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
+        //create token for the user to login using the id from db and a secret
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        //destructure the users data and send data without password to client
+        const { password : hashedPassword, ...rest} = validUser._doc;
+        //create expire date for the cookie
+        const expireDate = new Date(Date.new() + 3600000);
+        //send token as cookie to client
+        res.cookie('access_token', token, { httpOnly : true, expires : expireDate  }).status(200).json(rest);
+    } catch (error) {
+        //send error message to client if user is not signed in successfully
         next(error)
     }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js'
+import { signup } from '../controllers/auth.controller.js';
+import { signin } from '../controllers/auth.controller.js';
 
 
 //create express router 
@@ -7,5 +8,7 @@ const router = express.Router();
 
 //create route for signup 
 router.post('/signup', signup);
+//create route for signin
+router.post('/signin', signin);
 
 export default router;
